52a356ed988d4afbd51a1f934eda612d684158c7
e26396u <desireeferrigno@gmail.com>
2025-06-03 14:59:02 +0200

    RED

--- minesweeper.test.ts
+++ minesweeper.test.ts
@@ -1,16 +1,17 @@
 import { assertEquals } from "jsr:@std/assert";
 
 function minesweeper(minefield: string): string {
-
+  let compterBombe=0;
+for(let i of minefield){
   if(minefield =="*"){
-    minefield +="1"
-    console.log(minefield)
-    return minefield
-  }
-  return "";
-}
-let a = minesweeper("*")
+  compterBombe++
 
-Deno.test('Bombe (*)', () => {
-  assertEquals(a, "*1");
+  
+  }
+  return String(compterBombe);
+}return ""}
+let mine = minesweeper(".*.**.\n....*.\n..*...")
+console.log(mine)
+Deno.test('Chercher Bombe dans un tableau (*)', () => {
+  assertEquals(mine, "5");
 });
