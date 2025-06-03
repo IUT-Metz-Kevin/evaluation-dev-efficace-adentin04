65d310f5aa6f37e4eb7b59de0fb03fc1e76125a2
e26396u <desireeferrigno@gmail.com>
2025-06-03 16:38:16 +0200

    RED

--- minesweeper.test.ts
+++ minesweeper.test.ts
@@ -8,32 +8,57 @@ for(let i of minefield){
  
  if(i=="*"){
   compterBombe++
-}
+ 
+}  
   if(i=="." && minefield[compter-1]=== "*" ||minefield[compter+1]=== "*"){
-    minefield = minefield.replace(".",String(compterBombe))
+  
+    minefield = minefield.replace(".",String(2))
  
   }
     if(i=="*" && minefield[compter-1]=== "." ||minefield[compter+1]=== "*"){
-    minefield = minefield.replace(".",String(compterBombe))
+    minefield = minefield.replace(".",String(1))
    
   }
       if(i=="." && minefield[compter-1]=== "*" ||minefield[compter+1]=== "."){
-    minefield = minefield.replace(".",String(compterBombe))
+
+    minefield = minefield.replace(".",String(1))
+   
+  }
+      if(i=="." && minefield[compter-1]=== "*.." ||minefield[compter+3]=== "*" ||minefield[compter-3]=== "*..."){
+
+    minefield = minefield.replace(".",String(2))
    
   }
     if(i=="." && minefield[compter-1]=== "**" ||minefield[compter+1]=== "."){
-    minefield = minefield.replace(".",String(compterBombe))
+    minefield = minefield.replace(".",String(2))
+ 
+  }
+  if(i=="." && minefield[compter-1]=== ".*" ||minefield[compter+1]=== "*."){
+    minefield = minefield.replace(".",String(1))
 
   }
+    if(i=="." && minefield[compter-1]=== "." ||minefield[compter+1]=== "."){
+    minefield = minefield.replace(".",String(0))
+
+  }
+      if(i=="." && minefield[compter-1]=== "**." ||minefield[compter+2]=== "*."||minefield[compter-2]=== "**"){
+    minefield = minefield.replace(".",String(2))
+
+  }
+
 
 }
 
 
+ return (String(compterBombe),"",minefield);
 
-return minefield
 }
 let mine = minesweeper(".*.**.\n....*.\n..*...")
 console.log(mine)
 Deno.test('remplacer le nombre de point pas les numéro correct ', () => {
   assertEquals(mine, "1*2**2\n1234*2\n01*211");
+});
+
+Deno.test('Nombre Bombe', () => {
+  assertEquals(mine, "5");
 });
