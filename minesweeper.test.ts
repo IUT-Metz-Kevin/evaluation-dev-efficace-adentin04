7e11234c1accbcd16253c8f484ee1b3bd822ddf6
e26396u <desireeferrigno@gmail.com>
2025-06-03 14:50:21 +0200

    GREEN

--- minesweeper.test.ts
+++ minesweeper.test.ts
@@ -1,43 +1,16 @@
 import { assertEquals } from "jsr:@std/assert";
 
-function grille(){
-      let point = ".";
-       let bombe = "*"
-    let compterBombe=0;
-let ligne1= "*..";
-let ligne2= "..*";
-let ligne3="**.";       
-for(let i of ligne1){
+function minesweeper(minefield: string): string {
 
+  if(minefield =="."){
+    minefield ="0"
+    console.log(minefield)
+    return minefield
+  }
+  return "";
+}
+let a = minesweeper(".")
 
-if(i === "*"){
-  compterBombe++;
-
-}
-}
-return compterBombe
-}
-let testGrille = grille();
-Deno.test('Vérifier que il compte correctement les bombes', () => {
-  assertEquals(testGrille, 1);
+Deno.test('Case vide (.)', () => {
+  assertEquals(a, "0");
 });
-
-// class minesweeper{
-//     constructor(){}
-
-//     grille(){
-//         for(let i=0; i<3;i++){
-//             let point = "."
-      
-//               console.log(".");
-//               if(point === ".")
-//               {
-//                 console.log(0);
-//               }
-//         }
-//         for(let i=0;i<3;i++)
-//     }
-    
-// }
-// let a = new minesweeper()
-// a.grille()
